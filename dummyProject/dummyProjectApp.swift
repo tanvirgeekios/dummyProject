352ac6ef0a4d3aab1396011f84c078419e98520a
tanvir <tanvirgeek@gmail.com>
2020-12-03 14:52:37 +0600

    Initial Commit

---
+++ dummyProject/dummyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  dummyProjectApp.swift
+//  dummyProject
+//
+//  Created by MD Tanvir Alam on 3/12/20.
+//
+
+import SwiftUI
+
+@main
+struct dummyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
